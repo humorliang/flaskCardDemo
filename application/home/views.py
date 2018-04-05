1738cf7f32d3d617ec23696d7db32db96b57de89
humorliang <1174749673@qq.com>
2018-04-05 11:31:04 +0800

    databases models set success

--- application/home/views.py
+++ application/home/views.py
@@ -12,4 +12,28 @@ def index():
     # user = User(username='limin', password='12345')
     # db.session.add(user)
     # db.session.commit()
+
+    # admin = Admin(username='老王', password='123456')
+    # debt = Debt(credit_id='5656565', debt_date='2018-02-16', sum_money='100', cdid=1)
+    # consume = Consume(credit_id='3636565', consume_date='2018-02-16', sum_money='100')
+    # deal = Deal(credit_id='64585', sum_money='1000', deal_date='2018-03-06', deal_type='正常', description='转账')
+    # user = User(username='老李', password='256112', phone='188888888')
+    # info = Info(name="张三", age=20, phone='16699999998', sex=0, email='15369@163.com', job='程序员',
+    #              idcard='34242218620526568')
+    # credit = Credit(creditid='654238', limit='12000', overmoney='12000', creditname='老梁', vaildate='2018-2-13',
+    #                 cdtstatus=0,idcard='3425689595583')
+    # db.session.add_all([admin, debt, consume, deal, user, info, credit])
+    #
+    #
+    #
+    # db.session.add(admin)
+    # db.session.add(debt)
+    # db.session.commit()
+    user = User(name='老王', phone='123456789')
+    user2 = User(name='老李', phone='123456')
+    phone = Phone(name='老王手机', uid=0)
+    phone2 = Phone(name='老李手机', uid=1)
+    # db.session.add_all([user, user2, phone, phone2])
+    db.session.add(phone2)
+    db.session.commit()
     return render_template('home/index.html')
--- application/models.py
+++ application/models.py
@@ -2,17 +2,145 @@
 from application.exts import db
 
 
-# 创建表
-class User(db.Model):
-    __tablename__ = 'user'
+# 管理员表
+class Admin(db.Model):
+    __tablename__ = 'admin'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     username = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(100), nullable=False)
 
-
-    def __init__(self, id=None, username=None, password=None):
-        self.id = id
+    def __init__(self, username, password):
         self.username = username
         self.password = password
-        print('create table user')
 
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+# 还款表
+class Debt(db.Model):
+    __tablename__ = 'debt'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    credit_id = db.Column(db.String(30), db.ForeignKey('credit.credit_id'), nullable=True)
+    debt_date = db.Column(db.DateTime, nullable=False)
+    sum_money = db.Column(db.DECIMAL(12, 1), nullable=False)
+
+    def __init__(self, credit_id, debt_date, sum_money):
+        self.credit_id = credit_id
+        self.debt_date = debt_date
+        self.sum_money = sum_money
+
+    def __repr__(self):
+        return '<Debt %d>' % self.credit_id
+
+
+# 消费表
+class Consume(db.Model):
+    __tablename__ = 'consume'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    credit_id = db.Column(db.String(30), db.ForeignKey('credit.credit_id'), nullable=True)
+    consume_date = db.Column(db.DateTime, nullable=False)
+    sum_money = db.Column(db.DECIMAL(12, 1), nullable=False)
+
+    def __init__(self, credit_id, consume_date, sum_money):
+        self.credit_id = credit_id
+        self.consume_date = consume_date
+        self.sum_money = sum_money
+
+    def __repr__(self):
+        return '<Consume %d>' % self.credit_id
+
+
+# 信用卡表
+class Credit(db.Model):
+    __tablename__ = 'credit'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    credit_id = db.Column(db.String(30), nullable=False, unique=True)
+    limit = db.Column(db.String(20), nullable=False)
+    overMoney = db.Column(db.String(20), nullable=False)
+    creditName = db.Column(db.String(20), nullable=False)
+    phone = db.Column(db.Integer, db.ForeignKey('user.phone'), nullable=True)
+    vailDate = db.Column(db.DateTime)
+    cdt_status = db.Column(db.SmallInteger, nullable=False)  # 0 为冻结 1 为正常
+    idCard = db.Column(db.String(24), nullable=False)
+    # 还款表关系
+    debts = db.relationship('Debt', backref='credit', lazy='dynamic')
+    consumes = db.relationship('Consume', backref='credit', lazy='dynamic')
+    deals = db.relationship('Deal', backref='credit', lazy='dynamic')
+
+    def __init__(self, creditid, limit, overmoney, creditname, vaildate, cdtstatus, idcard):
+        self.credit_id = creditid
+        self.limit = limit
+        self.overMoney = overmoney
+        self.creditName = creditname
+        self.vailDate = vaildate
+        self.cdt_status = cdtstatus
+        self.idCard = idcard
+
+    def __repr__(self):
+        return '<Credit %s>' % self.creditName
+
+
+# 交易表
+class Deal(db.Model):
+    __tablename__ = 'deal'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    credit_id = db.Column(db.String(30), db.ForeignKey('credit.credit_id'), nullable=True)
+    sum_money = db.Column(db.DECIMAL(12, 1), nullable=False)
+    deal_date = db.Column(db.DateTime, nullable=False)
+    deal_type = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, credit_id, deal_date, deal_type, sum_money, description):
+        self.credit_id = credit_id
+        self.sum_money = sum_money
+        self.deal_date = deal_date
+        self.deal_type = deal_type
+        self.description = description
+
+    def __repr__(self):
+        return '<Deal %d>' % self.credit_id
+
+
+# 用户表
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    username = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.Integer, nullable=True, unique=True)  # 外键必须在主表中唯一或者为主键
+    # 用户表关系
+    info = db.relationship('Info', backref='user', lazy='dynamic', uselist=False)
+
+    def __init__(self, username, password, phone):
+        self.username = username
+        self.password = password
+        self.phone = phone
+
+    def __repr__(self):
+        return '<User %s>' % self.username
+
+
+# 用户信息表
+class Info(db.Model):
+    __tablename__ = 'info'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    phone = db.Column(db.Integer, db.ForeignKey('user.phone'), nullable=True)  # 手机号唯一
+    sex = db.Column(db.SmallInteger, nullable=False)  # 1 为男 0 为女
+    email = db.Column(db.String(20), nullable=False)
+    job = db.Column(db.String(10), nullable=False)
+    idCard = db.Column(db.String(24), nullable=False, unique=True)
+
+    def __init__(self, name, age, phone, sex, email, job, idcard):
+        self.name = name
+        self.age = age
+        self.phone = phone
+        self.sex = sex
+        self.email = email
+        self.job = job
+        self.idCard = idcard
+
+    def __repr__(self):
+        return '<Info %s>' % self.name
